9afb39f2dfcdbcc8b5a9f60dfcf712869c7ab43c
Cyril Auquier <cyrilauquier@hotmail.fr>
2024-10-08 16:22:09 +0200

    changer router system because web history is not supported by github pages

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # portfolio
 
 source code of my portfolio website
+
+## Pay attention
+
+- the routing system use the hash routing mode because the web history ins't supported inside github pages (i know it is not sexy but i don't have a choice)
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,11 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ProjectView from '../views/ProjectView.vue'
 import AllProjectView from '@/views/AllProjectView.vue'
 import AllSkillsView from '@/views/AllSkillsView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
