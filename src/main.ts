2b72331028c224e46d01f64c25f860bfed61348f
Cyril Auquier <cyrilauquier@hotmail.fr>
2024-10-20 20:10:37 +0200

    setup firebase

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import '@/modules/includes/firebase'
+
 const app = createApp(App)
 
 app.use(router)
---
+++ src/modules/includes/firebase.ts
@@ -0,0 +1,16 @@
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/firestore'
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  appId: import.meta.env.VITE_APP_ID
+}
+firebase.initializeApp(firebaseConfig)
+
+const db = firebase.firestore()
+const projectsCollection = db.collection('projects')
+
+export { db, projectsCollection }
